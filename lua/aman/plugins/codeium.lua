ea5ba7a6a24c4e4cfc4e34825cc86df9281c30d5
Amandeep Singh <amandeep.singh@avarni.co>
2024-07-27 23:57:50 +1000

    update codeium keymapping but not working

--- lua/aman/plugins/codeium.lua
+++ lua/aman/plugins/codeium.lua
@@ -1,7 +1,31 @@
--- Uncomment this after adding codeium plugin
--- keymap.set("i", "<C-Space>", "codeium#Accept()", { script = true, silent = true, nowait = true, expr = true, desc = "Accept Codeium completion" })
--- keymap.set("i", "<C-k>", "<cmd>call codeium#CycleCompletions(1)<cr>", { desc = "Cycle Codeium completions forward" })
--- keymap.set("i", "<C-j>", "<cmd>call codeium#CycleCompletions(-1)<cr>", { desc = "Cycle Codeium completions backward" })
--- keymap.set("i", "<C-x>", "<cmd>call codeium#Clear()<cr>", { desc = "Clear Codeium completion" })
-
-return {}
+return {
+	"Exafunction/codeium.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"hrsh7th/nvim-cmp",
+	},
+	config = function()
+		require("codeium").setup({
+			formatting = {
+				format = require("lspkind").cmp_format({
+					mode = "symbol",
+					maxwidth = 50,
+					ellipsis_char = "...",
+					symbol_map = { Codeium = "" },
+				}),
+			},
+		})
+		vim.keymap.set("i", "<C-Space>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-k>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-j>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true, silent = true })
+	end,
+}
