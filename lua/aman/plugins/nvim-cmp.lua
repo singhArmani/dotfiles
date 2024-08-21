90004e9aa8c3703f22b0678d264acbec2536db17
Amandeep Singh <amandeep.singh@avarni.co>
2024-08-21 16:44:48 +1000

    Add neo scroll for smooth scrolling

--- lua/aman/plugins/nvim-cmp.lua
+++ lua/aman/plugins/nvim-cmp.lua
@@ -58,9 +58,9 @@ return {
 			sources = cmp.config.sources({
 				{ name = "codeium" },
 				{ name = "nvim_lsp" },
-				{ name = "luasnip" }, -- snippets
 				{ name = "buffer" }, -- text within current buffer
 				{ name = "path" }, -- file system paths
+				{ name = "luasnip" }, -- snippets
 			}),
 			-- configure lspkind for vs-code like pictograms in completion menu
 			formatting = {
