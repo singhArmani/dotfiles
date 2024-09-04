7008c4e40a92488a492fb7496e0e18f797817d22
Amandeep Singh <amandeep.singh@avarni.co>
2024-09-04 22:24:55 +1000

    Fix mason config loading error

--- lua/aman/plugins/lsp/mason.lua
+++ lua/aman/plugins/lsp/mason.lua
@@ -1,54 +1,84 @@
 return {
-	"williamboman/mason.nvim",
-	dependencies = {
-		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
-	},
-	config = function()
-		-- import mason
-		local mason = require("mason")
+	-- Mason.nvim plugin
+	{
+		"williamboman/mason.nvim",
+		cmd = {
+			"Mason",
+			"MasonInstall",
+			"MasonUninstall",
+			"MasonUninstallAll",
+			"MasonLog",
+			"MasonUpdate",
+			"MasonUpdateAll",
+		},
+		config = function()
+			-- import mason
+			local mason = require("mason")
 
-		-- import mason-lspconfig
-		local mason_lspconfig = require("mason-lspconfig")
-
-		local mason_tool_installer = require("mason-tool-installer")
-
-		-- enable mason and configure icons
-		mason.setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗",
+			-- enable mason and configure icons
+			mason.setup({
+				ui = {
+					icons = {
+						package_installed = "✓",
+						package_pending = "➜",
+						package_uninstalled = "✗",
+					},
 				},
-			},
-		})
+			})
+		end,
+	},
 
-		mason_lspconfig.setup({
-			-- list of servers for mason to install
-			ensure_installed = {
-				-- "tsserver", (powered by typescript-tools.nvim)
-				"html",
-				"cssls",
-				"tailwindcss",
-				"lua_ls",
-				"graphql",
-				"emmet_ls",
-				"prismals",
-				"pyright",
-			},
-		})
+	-- Mason-lspconfig plugin
+	{
+		"williamboman/mason-lspconfig.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			-- import mason-lspconfig
+			local mason_lspconfig = require("mason-lspconfig")
 
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"prettier", -- prettier formatter
-				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
-				"csharpier", -- csharp formatter
-				"pylint", -- python linter
-				"eslint_d", -- js linter
-			},
-		})
-	end,
+			-- Setup mason-lspconfig
+			mason_lspconfig.setup({
+				-- list of servers for mason to install
+				ensure_installed = {
+					-- "tsserver", (powered by typescript-tools.nvim)
+					"html",
+					"cssls",
+					"tailwindcss",
+					"lua_ls",
+					"graphql",
+					"emmet_ls",
+					"prismals",
+					"pyright",
+				},
+			})
+		end,
+	},
+
+	-- Mason-tool-installer plugin
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		dependencies = {
+			"williamboman/mason.nvim",
+		},
+		config = function()
+			-- import mason-tool-installer
+			local mason_tool_installer = require("mason-tool-installer")
+
+			-- Setup mason-tool-installer
+			mason_tool_installer.setup({
+				ensure_installed = {
+					"prettier", -- prettier formatter
+					"stylua", -- lua formatter
+					"isort", -- python formatter
+					"black", -- python formatter
+					"csharpier", -- csharp formatter
+					"pylint", -- python linter
+					"eslint_d", -- js linter
+				},
+			})
+		end,
+	},
 }
