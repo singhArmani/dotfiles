51d2b94a06c85ce77dc6b7983beaf91339f90a29
Amandeep Singh <amandeep.singh@avarni.co>
2024-09-09 22:49:31 +1000

    update db ui plugin to add multiple connection

--- lua/aman/plugins/db-ui.lua
+++ lua/aman/plugins/db-ui.lua
@@ -16,17 +16,22 @@ return {
 		vim.g.db_ui_use_nerd_fonts = 1
 		vim.g.db_ui_show_help = 0
 		vim.g.db_ui_auto_execute_table_helpers = 1 -- auto execute table query when opening a table
-		vim.g.db_ui_win_position = "right"
+		vim.g.db_ui_win_position = "left"
 
 		-- Define your database connections here
 		vim.g.dbs = {
-			dev = "postgres://myuser:mypassword@localhost:54320/avarni-db",
-			-- prod = 'postgres://username:password@localhost:5432/prod_db',
+			{
+				name = "local",
+				url = "postgres://myuser:mypassword@localhost:54320/avarni-db",
+			},
+			{
+				name = "v2-main",
+				url = "postgres://avarni_core_api:9EBxHAuaZtxVdcKbogYRPCIx6DSV6W5k@dpg-cnoe8aacn0vc73bgqg6g-a.singapore-postgres.render.com:5432/avarni_core_api",
+			},
 		}
 
 		-- Set the location to save the connection details
-		vim.g.db_ui_save_location = "~/.config/nvim/db_ui"
-		vim.g.db_ui_save_location = "~/workspace/db_ui_queries"
+		vim.g.db_ui_save_location = "~/workspace/db_ui"
 		vim.keymap.set("n", "<leader>du", ":DBUIToggle<CR>", { noremap = true, silent = true })
 	end,
 }
