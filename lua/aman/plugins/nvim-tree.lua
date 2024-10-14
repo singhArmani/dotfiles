55ecbc077a83754c40e3c5e018951d0d4b6fdc68
Amandeep Singh <amandeep.singh@avarni.co>
2024-10-14 21:04:17 +1100

    remove leader>ee from nvim tree keymapping

--- lua/aman/plugins/nvim-tree.lua
+++ lua/aman/plugins/nvim-tree.lua
@@ -36,7 +36,6 @@ return {
 		-- set keymaps
 		local keymap = vim.keymap -- for conciseness
 
-		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
 		keymap.set("n", "<leader>u", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
 		keymap.set(
 			"n",
