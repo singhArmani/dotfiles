27026c633dfed12d66d2bb4148d141da3a898aba
Amandeep Singh <amandeep.singh@avarni.co>
2024-10-14 21:03:04 +1100

    use tiny code action for preview code action

---
+++ lua/aman/plugins/code-action.lua
@@ -0,0 +1,15 @@
+return {
+	"rachartier/tiny-code-action.nvim",
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ "nvim-telescope/telescope.nvim" },
+	},
+	event = "LspAttach",
+	config = function()
+		require("tiny-code-action").setup()
+
+		vim.keymap.set("n", "<leader>do", function()
+			require("tiny-code-action").code_action()
+		end, { noremap = true, silent = true })
+	end,
+}
