0e345b4f4ad9fb1fdf5c87a37a693eee30255841
Amandeep Singh <amandeep.singh@avarni.co>
2026-06-30 14:20:29 +1000

    Add more fonts and <leader>fi to search for git ignored files

--- lua/aman/plugins/fzf-lua.lua
+++ lua/aman/plugins/fzf-lua.lua
@@ -29,6 +29,10 @@ return {
 		local keymap = vim.keymap
 
 		keymap.set("n", ";", fzf.files, { desc = "Fuzzy find files in cwd" })
+		-- find files including hidden and gitignored ones (.git excluded)
+		keymap.set("n", "<leader>fi", function()
+			fzf.files({ fd_opts = "--color=never --type f --hidden --no-ignore --exclude .git" })
+		end, { desc = "Fuzzy find files (incl. gitignored)" })
 		keymap.set("n", "<leader>fr", fzf.oldfiles, { desc = "Fuzzy find recent files" })
 		keymap.set("n", "<leader>fs", fzf.live_grep, { desc = "Find string in cwd" })
 		keymap.set("n", "<leader>fc", fzf.grep_cword, { desc = "Find string under cursor in cwd" })
